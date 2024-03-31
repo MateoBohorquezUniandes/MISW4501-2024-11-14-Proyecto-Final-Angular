fadb90c969db50ece7fc1e7f3b6380de335c8e52
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-03-30 21:18:03 -0500

    Html de Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistryComponent } from './login/registry/registry.component';
+import { CalendarComponent } from './deportist/calendar/calendar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'registry', component: RegistryComponent},
+  { path: 'deportist/calendar', component: CalendarComponent}
+];
---
+++ src/app/deportist/deportist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deportist',
+  templateUrl: './deportist.component.html',
+  styleUrls: ['./deportist.component.css']
+})
+export class DeportistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
