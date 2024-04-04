8105c17ec4b43bb8b1b8cfa084fd03581c2db415
Melissa Castro <m.castros@uniandes.edu.co>
2024-04-04 00:02:42 -0500

    feat(planes): cards and styles

---
+++ src/app/plan/plan-list/plan-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-list',
+  templateUrl: './plan-list.component.html',
+  styleUrls: ['./plan-list.component.css',
+  '../../../styles.css']
+})
+export class PlanListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
