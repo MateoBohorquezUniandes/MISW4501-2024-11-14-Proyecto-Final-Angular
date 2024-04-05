2afda56499429b39d32cc458b81863e69a384f36
Melissa Castro <m.castros@uniandes.edu.co>
2024-04-04 21:31:59 -0500

    feat(plans): unit test plan-list component

--- src/app/plan/plan-list/plan-list.component.spec.ts
+++ src/app/plan/plan-list/plan-list.component.spec.ts
@@ -8,6 +8,7 @@ import { PlanListComponent } from './plan-list.component';
 describe('PlanListComponent', () => {
   let component: PlanListComponent;
   let fixture: ComponentFixture<PlanListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,9 +21,22 @@ describe('PlanListComponent', () => {
     fixture = TestBed.createComponent(PlanListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 3 <div.card-body> elements', () => {
+    expect(debug.queryAll(By.css('div.card-body'))).toHaveSize(3)
+  });
+
+  it('should have 3 <h5.card-title>', () => {
+      expect(debug.queryAll(By.css('h5.card-title'))).toHaveSize(3)
+  });
+
+  it('should have 3 <div.card-footer> elements', () => {
+    expect(debug.queryAll(By.css('div.card-footer'))).toHaveSize(3)
   });
 });
