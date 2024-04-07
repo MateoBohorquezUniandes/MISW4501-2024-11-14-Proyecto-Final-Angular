98bdedda48db9f923091f24b628baad0ed56a292
Helena-Pat29 <h.patarroyo@uniandes.edu.co>
2024-04-07 11:19:21 -0500

    Feat/planesafiliaciontoboard (#4)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { RegistroComponent } from './login/registro/registro.component';
 import { PlanListComponent } from './plan/plan-list/plan-list.component';
+import { DeportistaComponent } from './deportista/deportista.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent},
   { path: 'registry', component: RegistroComponent },
-  { path: 'plans', component: PlanListComponent}
+  { path: 'plans', component: PlanListComponent},
+  { path: 'deportista', component: DeportistaComponent }
 ];
