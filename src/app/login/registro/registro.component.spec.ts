c9238005894397e5d9a104f8f2dce5154ecef2c8
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-04-13 16:37:56 -0500

    feat(HU001): Presentación Registro

--- src/app/login/registro/registro.component.spec.ts
+++ src/app/login/registro/registro.component.spec.ts
@@ -2,15 +2,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { ToastrModule } from 'ngx-toastr';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RegistroComponent } from './registro.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('RegistroComponent', () => {
   let component: RegistroComponent;
   let fixture: ComponentFixture<RegistroComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[ToastrModule.forRoot(),HttpClientTestingModule, ReactiveFormsModule],
       declarations: [RegistroComponent],
     }).compileComponents();
   }));
@@ -19,9 +23,97 @@ describe('RegistroComponent', () => {
     fixture = TestBed.createComponent(RegistroComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Debe ser valido el registro', () => {
+    const nombre = component.registroForm.controls['nombre'];
+    const apellido = component.registroForm.controls['apellido'];
+    const tipo_doc = component.registroForm.controls['tipo_doc'];
+    const documento = component.registroForm.controls['documento'];
+    const contrasena = component.registroForm.controls['contrasena'];
+    const contrasena2 = component.registroForm.controls['contrasena2'];
+    const genero = component.registroForm.controls['genero'];
+    const edad = component.registroForm.controls['edad'];
+    const peso = component.registroForm.controls['peso'];
+    const altura = component.registroForm.controls['altura'];
+    const pais_nacimiento = component.registroForm.controls['pais_nacimiento'];
+    const ciudad_nacimiento = component.registroForm.controls['ciudad_nacimiento'];
+    const pais_vivienda = component.registroForm.controls['pais_vivienda'];
+    const ciudad_vivienda = component.registroForm.controls['ciudad_vivienda'];
+    const tiempo = component.registroForm.controls['tiempo'];
+    const deporte = component.registroForm.controls['deporte'];
+    nombre.setValue('Gustavo');
+    apellido.setValue('Barbosa');
+    tipo_doc.setValue(1);
+    documento.setValue('37864172');
+    contrasena.setValue('mocos123');
+    contrasena2.setValue('mocos123');
+    genero.setValue(1);
+    edad.setValue('36');
+    peso.setValue('63');
+    altura.setValue('180');
+    pais_nacimiento.setValue('Brasil');
+    ciudad_nacimiento.setValue('Sao Pablo');
+    pais_vivienda.setValue('Colombia');
+    ciudad_vivienda.setValue('Medellin');
+    tiempo.setValue('9');
+    deporte.setValue(1);
+
+    expect(component.registroForm.valid).toBeTrue();
+
+    let boton = debug.query(By.css('#boton_registro'));
+    boton.nativeElement.click();
+  });
+
+  it('Debe ser invalido el registro por contraseñas inconsistentes', () => {
+    const nombre = component.registroForm.controls['nombre'];
+    const apellido = component.registroForm.controls['apellido'];
+    const tipo_doc = component.registroForm.controls['tipo_doc'];
+    const documento = component.registroForm.controls['documento'];
+    const contrasena = component.registroForm.controls['contrasena'];
+    const contrasena2 = component.registroForm.controls['contrasena2'];
+    const genero = component.registroForm.controls['genero'];
+    const edad = component.registroForm.controls['edad'];
+    const peso = component.registroForm.controls['peso'];
+    const altura = component.registroForm.controls['altura'];
+    const pais_nacimiento = component.registroForm.controls['pais_nacimiento'];
+    const ciudad_nacimiento = component.registroForm.controls['ciudad_nacimiento'];
+    const pais_vivienda = component.registroForm.controls['pais_vivienda'];
+    const ciudad_vivienda = component.registroForm.controls['ciudad_vivienda'];
+    const tiempo = component.registroForm.controls['tiempo'];
+    const deporte = component.registroForm.controls['deporte'];
+    nombre.setValue('Gustavo');
+    apellido.setValue('Barbosa');
+    tipo_doc.setValue(1);
+    documento.setValue('37864172');
+    contrasena.setValue('mocos123');
+    contrasena2.setValue('mocos1234');
+    genero.setValue(1);
+    edad.setValue('36');
+    peso.setValue('63');
+    altura.setValue('180');
+    pais_nacimiento.setValue('Brasil');
+    ciudad_nacimiento.setValue('Sao Pablo');
+    pais_vivienda.setValue('Colombia');
+    ciudad_vivienda.setValue('Medellin');
+    tiempo.setValue('9');
+    deporte.setValue(1);
+
+    expect(component.registroForm.valid).toBeTrue();
+
+    let boton = debug.query(By.css('#boton_registro'));
+    boton.nativeElement.click();
+  });
+
+  it('Debe poder volver a login', () => {
+    expect(debug.query(By.css('#boton_volver'))).toHaveSize(1);
+    let boton = debug.query(By.css('#boton_volver'));
+    boton.nativeElement.click();
+  });
 });
+
--- src/app/login/registro/registro.component.ts
+++ src/app/login/registro/registro.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { LoginService } from '../login.service';
 import { Router } from '@angular/router';
-import { error } from 'console';
 
 @Component({
   selector: 'app-registry',
@@ -15,8 +11,8 @@ import { error } from 'console';
 })
 export class RegistroComponent implements OnInit {
   registroForm = this.formBuilder.group({
-    nombre:['',[]],
-    apellido: ['',[]],
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    apellido: ['', [Validators.required, Validators.minLength(3)]],
     tipo_doc: [0, [Validators.required, Validators.min(1)]],
     documento: [
       '',
@@ -24,21 +20,68 @@ export class RegistroComponent implements OnInit {
         Validators.required,
         Validators.minLength(7),
         Validators.maxLength(20),
+        Validators.pattern('^[0-9]*$'),
       ],
     ],
-    contrasena: ['', [Validators.required]],
-    contrasena2: ['',[]],
-    genero:['',[]],
-    edad:['',[]],
-    peso:['',[]],
-    altura:['',[]],
-    pais_nacimiento:['',[]],
-    ciudad_nacimiento:['',[]],
-    pais_vivienda:['',[]],
-    ciudad_vivienda:['',[]],
-    tiempo:['',[]],
-    deporte:['',[]]
-  })
+    contrasena: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ],
+    ],
+    contrasena2: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ],
+    ],
+    genero: [0, [Validators.required, Validators.min(1)]],
+    edad: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(3),
+        Validators.pattern('^[0-9]*$'),
+      ],
+    ],
+    peso: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(3),
+        Validators.pattern('^[0-9]*$'),
+      ],
+    ],
+    altura: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(3),
+        Validators.pattern('^[0-9]*$'),
+      ],
+    ],
+    pais_nacimiento: ['', [Validators.required]],
+    ciudad_nacimiento: ['', [Validators.required]],
+    pais_vivienda: ['', [Validators.required]],
+    ciudad_vivienda: ['', [Validators.required]],
+    tiempo: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(3),
+        Validators.pattern('^[0-9]*$'),
+      ],
+    ],
+    deporte: [0, [Validators.required, Validators.min(1)]],
+  });
   constructor(
     private formBuilder: FormBuilder,
     private toastr: ToastrService,
@@ -46,19 +89,73 @@ export class RegistroComponent implements OnInit {
     private router: Router
   ) {}
 
-  registrase(
+  validarContrasena(){
+      const control = this.registroForm.controls.contrasena;
+      const matchingControl = this.registroForm.controls.contrasena2;
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ confirmedValidator: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+  }
 
-  ){
-    const registro = {
+  registrarse(
+    nombre: string | null,
+    apellido: string | null,
+    tipo_doc: number | null,
+    documento: string | null,
+    contrasena: string | null,
+    genero: number | null,
+    edad: string | null,
+    peso: string | null,
+    altura: string | null,
+    pais_nacimiento: string | null,
+    ciudad_nacimiento: string | null,
+    pais_vivienda: string | null,
+    ciudad_vivienda: string | null,
+    tiempo: string | null,
+    deporte: number | null
+  ) {
+    this.validarContrasena()
+    if(this.registroForm.controls.contrasena2.errors == null){
+      const registro = {
+        identificacion:{
+          tipo: tipo_doc,
+          valor: documento
+        },
+        nombre: nombre,
+        apellido: apellido,
+        rol: 'DEPORTISTA',
+        contrasena: contrasena,
+        demografia:{
+          pais_nacimiento: pais_nacimiento,
+          ciudad_nacimiento: ciudad_nacimiento,
+          pais_residencia: pais_vivienda,
+          ciudad_residencia: ciudad_vivienda,
+          tiempo_residencia: tiempo,
+          genero: genero,
+          edad: edad,
+          peso: peso,
+          altura: altura,
+        },
+        deportes:[{deporte}]
+      };
+      this.loginService.registrarse(registro).subscribe(
+        (data) => {
+          this.router.navigateByUrl('/login');
+        },
+        (error) => {
+          this.toastr.error(
+            'Error',
+            'Error registrando el usuario:' + error.message
+          );
+        }
+      );
+    }
+  }
 
-    };
-    this.loginService.registrarse(registro).subscribe(
-      data => {
-        this.router.navigateByUrl("/login")
-      },
-    error => {
-      this.toastr.error("Error", "Error registrando el usuario:" + error)
-    });
+  public volver(){
+    this.router.navigateByUrl('/login');
   }
 
   ngOnInit() {}
