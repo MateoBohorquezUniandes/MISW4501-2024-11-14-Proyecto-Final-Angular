d11b46eb1cf1d55e58cdac4e0b0ccb4e2686a537
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-04-17 17:11:13 -0500

    feat(HUNA): Cambio URL Ingress

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
-const UrlUsuarios='http://34.102.132.151/';
-const UrlPerfiles='http://34.102.132.151/'; //Aqui va la url de ingress
+const UrlUsuarios='http://34.49.240.237/';
+const UrlPerfiles='http://34.49.240.237/'; //Aqui va la url de ingress
 export const environment = {
   production: true,
   UrlUsuarios,
