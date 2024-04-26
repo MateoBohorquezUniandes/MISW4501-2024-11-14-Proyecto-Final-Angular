3fb72ed22457fccbc9ef68844c8bbba6939bd429
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-04-25 22:25:57 -0500

    feat(HUNA): Se añaden Headers

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { CookieService } from "ngx-cookie-service";
@@ -11,8 +11,20 @@ export class LoginService {
   private apiUrl = environment.UrlUsuarios + 'usuarios/commands';
   constructor(private http:HttpClient, private cookies: CookieService) {}
 
+  createHeaders() {
+    const token = this.cookies.get('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+    });
+    return headers;
+  }
+
   login(request: any): Observable<any> {
-    return this.http.post<string>(this.apiUrl + '/login',request)
+    let header = this.createHeaders();
+    return this.http.post<string>(this.apiUrl + '/login',request,{ headers: header } )
   }
   setToken(token: string){
     this.cookies.set("token", token);
@@ -21,7 +33,8 @@ export class LoginService {
     return this.cookies.get("token");
   }
   registrarse(request: any): Observable<any>{
-    return this.http.post<string>(this.apiUrl, request);
+    let header = this.createHeaders();
+    return this.http.post<string>(this.apiUrl, request ,{ headers: header });
   }
   deleteToken(){
     this.cookies.delete("token");
