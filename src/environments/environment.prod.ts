01719f3c99645fb280d97fdaad176870f53312cc
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-04-25 21:15:26 -0500

    feat(HUNA): Ajuste url ingress

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
-const UrlUsuarios='http://34.49.240.237/';
-const UrlPerfilesCommand = 'http://34.49.240.237/perfiles/commands'; //Aqui va la url de ingress
+const UrlUsuarios='http://34.36.129.27/';
+const UrlPerfiles='http://34.36.129.27/'; //Aqui va la url de ingress
 export const environment = {
   production: true,
   UrlUsuarios,
-  UrlPerfilesCommand,
+  UrlPerfiles,
 };
