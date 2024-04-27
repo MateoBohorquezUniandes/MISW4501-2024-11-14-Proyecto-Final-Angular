841b1c28b5897105bca09fbb28b16eab1ae76e14
Melissa Castro <m.castros@uniandes.edu.co>
2024-04-27 13:16:24 -0500

    feat(molestias): environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 const UrlUsuarios='http://34.49.240.237/';
-const UrlPerfiles='http://34.49.240.237/'; //Aqui va la url de ingress
+const UrlPerfilesCommand = 'http://34.49.240.237/perfiles/commands'; //Aqui va la url de ingress
 export const environment = {
   production: true,
   UrlUsuarios,
-  UrlPerfiles
+  UrlPerfilesCommand,
 };
