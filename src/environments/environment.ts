f41d680dadbfb02e6f9141e8742dac105919834c
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-05-11 10:35:49 -0500

    feat(HUNA): change language (#22)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,13 @@
 const UrlUsuarios = 'http://localhost:3011/usuarios';
 const UrlPerfiles = 'http://127.0.0.1:3021/perfiles'; //Aqui va la url de ingress
 const UrlIndicador = 'http://127.0.0.1:3060/indicadores';
+const UrlES=`http://localhost:4200`;
+const UrlEN=`http://localhost:4201`;
 export const environment = {
   production: true,
   UrlUsuarios,
   UrlPerfiles,
   UrlIndicador,
+  UrlES,
+  UrlEN
 };
