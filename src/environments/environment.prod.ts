8b4b427f77c0d2662f5b1452459f7cb52f69845e
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-05-11 10:27:39 -0500

    feat(HUNA): Ajustes cambio lenguaje

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,14 @@ const baseUrl = 'http://34.36.129.27';
 
 const UrlUsuarios=`${baseUrl}/usuarios`;
 const UrlPerfiles=`${baseUrl}/perfiles`; //Aqui va la url de ingress
+const UrlES=`https://angular2-5dafvog5ta-uc.a.run.app`;
+const UrlEN=`https://angular3-5dafvog5ta-uc.a.run.app`;
 export const environment = {
   production: true,
   UrlUsuarios,
   UrlPerfiles,
+  UrlES,
+  UrlEN
 };
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,11 @@
 const UrlUsuarios = 'http://localhost:3011/usuarios';
 const UrlPerfiles = 'http://127.0.0.1:3021/perfiles'; //Aqui va la url de ingress
+const UrlES=`http://localhost:4200`;
+const UrlEN=`http://localhost:4201`;
 export const environment = {
   production: true,
   UrlUsuarios,
   UrlPerfiles,
+  UrlES,
+  UrlEN
 };
