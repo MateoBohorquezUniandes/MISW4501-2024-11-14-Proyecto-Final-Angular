b5efbf89385a6126a2bcd8aad0295b8f0f091f48
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-05-14 15:22:10 -0500

    feat(HU042): Ajustes HU042

--- src/app/deportista/deportista.module.ts
+++ src/app/deportista/deportista.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DeportistaHomeComponent } from './deportista-home/deportista-home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { PerfilesModule } from './perfiles/perfiles.module';
 
@@ -9,9 +9,9 @@ import { PerfilesModule } from './perfiles/perfiles.module';
     CommonModule,
     RouterModule,
     PerfilesModule,
-    DeportistaHomeComponent
+    DashboardComponent
   ],
-  //declarations: [DeportistaHomeComponent],
+  declarations: [],
   exports: [PerfilesModule]
 })
 export class DeportistaModule { }
